fe136b0bced607f50b2b3754a4326f9a3972a25f
Timothy Mai <timothy.mai13@gmail.com>
2020-02-11 21:31:57 -0800

    added styling for InputBar inside of ChatboxContainer

--- src/containers/ChatboxContainer.jsx
+++ src/containers/ChatboxContainer.jsx
@@ -10,12 +10,27 @@
  */
 
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import InputBar from '../components/InputBar';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  inputBar: {
+    position: 'fixed',
+    bottom: 0,
+    width: `calc(100% - ${drawerWidth}px - 50px)`,
+    height: '90px',
+    'background-color': 'white',
+  },
+}));
+
 function ChatboxContainer() {
+  const classes = useStyles();
+
   return (
     <div>
-      <InputBar />
+      <InputBar className={classes.inputBar} />
     </div>
   );
 }
