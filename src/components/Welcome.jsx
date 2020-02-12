70a6c473fc83bbf14f59d253d982920f9f5dc824
Timothy Mai <timothy.mai13@gmail.com>
2020-02-11 20:50:55 -0800

    added basic components to Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,12 +10,37 @@
  */
 
 import React from 'react';
+// import PropTypes from 'prop-types';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+
+});
 
 function Welcome() {
   return (
-    <div>
-      welcome
-    </div>
+    <Container>
+      <div>
+        <Typography variant="h4">
+          Welcome! What is your name?
+        </Typography>
+        <form>
+          <TextField
+            id="name"
+            label="Name"
+            fullWidth
+            
+          />
+          <Button>
+            Submit
+          </Button>
+        </form>
+      </div>
+    </Container>
   );
 }
 
