5ace3ea22d8f6e45dfac5f1e282864d991ab0223
Timothy Mai <timothy.mai13@gmail.com>
2020-02-11 23:29:51 -0800

    added routes via react router

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@
  */
 
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import ChatroomContainer from './containers/ChatroomContainer';
 
@@ -18,10 +23,24 @@ function App() {
   const [username, setUsername] = useState('friend');
 
   return (
-    <div>
-      {/* <Welcome setUsername={setUsername} /> */}
-      <ChatroomContainer pageName="Home" hasChatbox={true} username={username} />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/home">
+            <ChatroomContainer pageName="Home" hasChatbox={false} username={username} />
+          </Route>
+          <Route path="/general">
+            <ChatroomContainer pageName="General" hasChatbox username={username} />
+          </Route>
+          <Route path="/ideas">
+            <ChatroomContainer pageName="Ideas" hasChatbox username={username} />
+          </Route>
+          <Route path="/">
+            <Welcome setUsername={setUsername} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
