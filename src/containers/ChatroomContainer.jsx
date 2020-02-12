6e572972b9bb45658b69f6d69dc9df0126300685
Timothy Mai <timothy.mai13@gmail.com>
2020-02-11 21:11:56 -0800

    added structure and styling to ChatroomContainer

--- src/containers/ChatroomContainer.jsx
+++ src/containers/ChatroomContainer.jsx
@@ -10,11 +10,97 @@
  */
 
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline'; // add to main app
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 240;
+
+// Material-UI uses a 'CSS-in-JS' approach to styling
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 function ChatroomContainer() {
+  // this is used for Material-UI to access the CSS we defined above
+  const classes = useStyles();
+
   return (
-    <div>
-      chatroom container
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button key="Home">
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button key="General">
+            <ListItemText primary="General" />
+          </ListItem>
+          <ListItem button key="Ideas">
+            <ListItemText primary="Ideas" />
+          </ListItem>
+        </List>
+        <Divider />
+        <Divider />
+        <List>
+          <ListItem>
+            <ListItemText>
+              Random questions asked:
+            </ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemText>
+              0
+            </ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        main content
+      </main>
     </div>
   );
 }
