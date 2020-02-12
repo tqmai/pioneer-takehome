e5077a98986f184d778bb23a139826f285ba3a2d
Timothy Mai <timothy.mai13@gmail.com>
2020-02-11 21:19:38 -0800

    added basic components to InputBar

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -10,11 +10,24 @@
  */
 
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function InputBar() {
   return (
     <div>
-      input bar
+      <form>
+        <span>
+          <TextField />
+          <Button>
+            Random Question!
+          </Button>
+          <Button>
+            Send
+          </Button>
+        </span>
+      </form>
     </div>
   );
 }
