1ca16de3d07b155d0250718e85ec13f2b7087a68
Timothy Mai <timothy.mai13@gmail.com>
2020-02-11 20:35:08 -0800

    added description to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+/* eslint-disable react/jsx-filename-extension */
+/**
+ * ************************************
+ *
+ * @module  index.jsx
+ * @author Timothy Mai
+ * @date 2/11/20
+ * @description entry point for application.  Hangs React app off of #root in index.html
+ *
+ * ************************************
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
